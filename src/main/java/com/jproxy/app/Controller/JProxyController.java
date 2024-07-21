985f60628a3ac9273ec5d9798ed5e7a5daa315db
Pedro-IFC <pdanieldeoliveirahoeller@gmail.com>
2024-07-21 16:35:53 -0300

    Docker add

--- src/main/java/com/jproxy/app/Controller/JProxyController.java
+++ src/main/java/com/jproxy/app/Controller/JProxyController.java
@@ -2,7 +2,6 @@ package com.jproxy.app.Controller;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
