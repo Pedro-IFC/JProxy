b7e839215b8dfcf1dfaad6afcac59a6a041ea430
Pedro-IFC <pdanieldeoliveirahoeller@gmail.com>
2024-07-26 14:00:04 -0300

    Logs + Clients

--- src/main/java/com/jproxy/app/Service/ProdutoService.java
+++ src/main/java/com/jproxy/app/Service/ProdutoService.java
@@ -18,18 +18,25 @@ public class ProdutoService {
 	}
 	public Produto getProduto(int id) {
 		ProdutoTimestamp prod = ProdutoCacheado.getInstance().getById(id);
+        System.out.println("Verifica se o produto está em cache");
 		if(prod!=null) {
-	        Instant now = Instant.now();
+	        System.out.println("Está em cache");
+			Instant now = Instant.now();
 	        Instant prodTimestampInstant = Instant.ofEpochSecond(prod.getTimestamp().getSeconds(), prod.getTimestamp().getNanos());
-	        Instant fiveMinutesAgo = now.minusSeconds(5 * 60);
+	        Instant fiveMinutesAgo = now.minusSeconds(20);
+	        System.out.println("Verifica se o produto está no tempo válido");
 	        if (prodTimestampInstant.isAfter(fiveMinutesAgo)) {
+		        System.out.println("Está inválido e atualiza antes de retornar");
 	    		Produto findById=produtoRepo.findById(id).orElse(null);
 	    		prod.atualizar(findById);
 	    		return findById;
 	        }
+	        System.out.println("Está válido");
 	        return prod.getProduto();
 		}
+        System.out.println("Não está em cache");
 		Produto findById=produtoRepo.findById(id).orElse(null);
+        System.out.println("Adiciona ao cache");
 		ProdutoCacheado.getInstance().getProdutos().add(new ProdutoTimestamp(findById));
         return findById;
 	}
